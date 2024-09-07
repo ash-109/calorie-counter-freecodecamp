f6dffbaa4bc331fbb2f8b5c415a0168af3feb136
Ashvin Antyah <a.antyah@gmail.com>
2024-09-07 17:43:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,12 @@ return str.match(regex);
 function addEntry() {
     const targetId = "#" + entryDropdown.value
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name </label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories" >Entry ${entryNumber} Calories</label>
+    <input type="number" placeholder="Calories" min="0" id="${entryDropdown.value}-${entryNumber}-calories">
+    
+    `
 }
